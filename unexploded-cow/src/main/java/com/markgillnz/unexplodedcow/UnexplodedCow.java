f1e7841e8f9de86a8ad6ae2c620594ca03243891
Mark Gill <markgillnz@gmail.com>
2019-06-19 19:40:49 +1200

    Initial Project Setup

---
+++ unexploded-cow/src/main/java/com/markgillnz/unexplodedcow/UnexplodedCow.java
@@ -0,0 +1,11 @@
+package com.markgillnz.unexplodedcow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnexplodedCow {
+    public static void main( String[] args ) {
+        SpringApplication.run(UnexplodedCow.class, args);
+    }
+}
